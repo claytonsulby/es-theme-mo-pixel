d223d11c85070e883d2b13b193a86d24de1f99d8
Clayton Sulby <cls6275@psu.edu>
2022-01-08 16:16:18 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+then echo "Please run as root (sudo)"
+	exit 1
+fi
+
+if [[ $1 != "" ]] ; then
+	BRANCH=$1
+else
+	BRANCH="master"
+fi
+
+# Update git
+echo "Updating from git.."
+git reset --hard HEAD
+git pull
+git checkout $BRANCH
+
+# Set permissions on the initial required
+chmod +x install.sh
+
+# Move splashscreens to folder
+echo "Moving ./splashscreens to ~/RetroPie/"
+cp -auv ./splashscreens ~/RetroPie
+
+echo "Moving ./screensavers to ~/RetroPie/"
+cp -auv ./screensavers ~/RetroPie
+
+echo "Moving ./videos to ~/RetroPie/"
+cp -auv ./videos ~/RetroPie
